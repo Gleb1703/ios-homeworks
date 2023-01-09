5459c2987c468ad635fb50649babdeb313e34cec
Glebio <nightmare090@mail.ru>
2023-01-09 21:03:19 +0300

    last

--- Navigation/PostViewController/PostViewController.swift
+++ Navigation/PostViewController/PostViewController.swift
@@ -11,6 +11,12 @@ class PostViewController: UIViewController {
     override func viewDidLoad() {
         view.backgroundColor = .yellow
         self.navigationItem.title = titlePost
+        let myButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(tap))
+        navigationItem.rightBarButtonItem = myButton
     }
     var titlePost = "Мой пост"
+    @objc func tap() {
+        let infoViewController = InfoViewController()
+      navigationController?.pushViewController(infoViewController, animated: true)
+    }
 }
--- Navigation/ProfileViewController/ProfileViewController.swift
+++ Navigation/ProfileViewController/ProfileViewController.swift
@@ -10,11 +10,12 @@ import UIKit
 class ProfileViewController: UIViewController {
     override func viewDidLoad() {
         view.backgroundColor = .purple
-        let myButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(tap))
-        navigationItem.rightBarButtonItem = myButton
+//        let myButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(tap))
+//        navigationItem.rightBarButtonItem = myButton
     }
-    @objc func tap() {
+/*    @objc func tap() {
         let infoViewController = InfoViewController()
       navigationController?.pushViewController(infoViewController, animated: true)
     }
+*/
 }
