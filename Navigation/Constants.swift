9d0c47b0689c2cd7025b3dee2e47f5390e3f5734
Glebio <nightmare090@mail.ru>
2023-03-21 22:47:40 +0300

    try

---
+++ Navigation/Constants.swift
@@ -0,0 +1,8 @@
+//
+//  Constants.swift
+//  Navigation
+//
+//  Created by gleb on 21/03/2023.
+//
+
+import Foundation
---
+++ Navigation/TextFieldWithPadding.swift
@@ -0,0 +1,8 @@
+//
+//  TextFieldWithPadding.swift
+//  Navigation
+//
+//  Created by gleb on 21/03/2023.
+//
+
+import Foundation
