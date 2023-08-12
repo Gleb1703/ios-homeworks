76b48d3ae7fd562b67850f02b126dbe4783e2ad5
Glebio <nightmare090@mail.ru>
2023-08-12 06:36:39 +0300

    3.7ALL2

--- Navigation/Profile/ProfileViewController.swift
+++ Navigation/Profile/ProfileViewController.swift
@@ -14,6 +14,7 @@ class ProfileViewController: UIViewController {
     private var posts: [PostModel] = PostModel.make()
     private var cellIndex = 0
 
+
     var userService: UserService
     var login: String?
     
